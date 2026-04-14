bc41014bcdad72a903923d41f43be850b1aff814
minhdang241 <d.baminh@gmail.com>
2026-04-15 07:03:25 +1000

    feat: replace LogoIcon with base64 encoded logo image in OpengraphImage component

--- components/opengraph-image.tsx
+++ components/opengraph-image.tsx
@@ -1,5 +1,4 @@
 import { ImageResponse } from "next/og";
-import LogoIcon from "./icons/logo";
 import { join } from "path";
 import { readFile } from "fs/promises";
 
@@ -17,14 +16,18 @@ export default async function OpengraphImage(
     ...props,
   };
 
-  const file = await readFile(join(process.cwd(), "./fonts/Inter-Bold.ttf"));
-  const font = Uint8Array.from(file).buffer;
+  const [fontFile, logoFile] = await Promise.all([
+    readFile(join(process.cwd(), "./fonts/Inter-Bold.ttf")),
+    readFile(join(process.cwd(), "./public/images/logo-lbn.png")),
+  ]);
+  const font = Uint8Array.from(fontFile).buffer;
+  const logoSrc = `data:image/png;base64,${logoFile.toString("base64")}`;
 
   return new ImageResponse(
     (
       <div tw="flex h-full w-full flex-col items-center justify-center bg-black">
         <div tw="flex flex-none items-center justify-center border border-neutral-700 h-[160px] w-[160px] rounded-3xl">
-          <LogoIcon width="64" height="58" fill="white" />
+          <img src={logoSrc} width="100" height="100" style={{ objectFit: "contain" }} />
         </div>
         <p tw="mt-12 text-6xl font-bold text-white">{title}</p>
       </div>
